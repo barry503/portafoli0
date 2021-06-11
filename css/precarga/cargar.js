c7feffedfab71f5ba32f7dc29bb6ad42d7960f0e
Jose Ramirez <momar5618@gmail.com>
2021-06-10 23:20:43 -0600

    Add files via upload

---
+++ css/precarga/cargar.js
@@ -0,0 +1,20 @@
+/* precargado de la pagina */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+
+    });
+
+   
+
+})(jQuery);
+
+
